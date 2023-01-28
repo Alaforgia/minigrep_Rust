23577129e9ac3687052efe711cff49da7c2234f3
Tony LaForgia <alforgia428@gmail.com>
2023-01-27 19:35:19 -0600

    args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::env;
 fn main() {
+// The program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
  let args: Vec<String> = env::args().collect();
- println!("{:?}", args);
+ 
+ let query = &args[1];
+ let filename = &args[2];
+
+ println!("Searching for {}", query);
+ println!("In file {}", filename)
 }
