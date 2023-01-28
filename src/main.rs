35934a3e323adc4c9127491607351fb617cfb25c
Tony LaForgia <alforgia428@gmail.com>
2023-01-27 19:42:18 -0600

    txt output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs; // handles files
 fn main() {
 // The program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
  let args: Vec<String> = env::args().collect();
@@ -7,5 +8,10 @@ fn main() {
  let filename = &args[2];
 
  println!("Searching for {}", query);
- println!("In file {}", filename)
+ println!("In file {}", filename);
+
+ // fs::read_to_string takes the filename, opens that file, and returns a Result<String> of the file’s contents.
+ let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+ println!("With text: \n{}", contents);
 }
