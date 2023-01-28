9af1128ea6ba6fbbdbab782934ff1dab9b63ed4f
Tony LaForgia <alforgia428@gmail.com>
2023-01-27 20:03:51 -0600

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs; // handles files
+use std::process;
 fn main() {
 // The program’s name takes up the first value in the vector at args[0], so we’re starting at index 1.
  let args: Vec<String> = env::args().collect();
 
- let config = Config::new(&args);
+ let config = Config::new(&args).unwrap_or_else(|err| {
+     println!("Problem parsing arguments: {}", err);
+     process::exit(1);
+    });
  
 
  println!("Searching for {}", config.query);
@@ -21,11 +25,14 @@ struct Config {
     filename: String,
 }
 
-impl Config{
-    fn new(args: &[String]) -> Config {
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
